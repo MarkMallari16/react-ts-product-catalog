d5654d9fdc4eef3a937f637fdc9e509e779ae011
Mark Christian Mallari <mallariftw01@gmail.com>
2025-07-09 21:48:32 +0800

    upd: Remove unecessary import

--- src/component/Header.tsx
+++ src/component/Header.tsx
@@ -1,6 +1,5 @@
 import { Link } from 'react-router'
 import { useCart } from '../context/CartContext'
-import MarkProfile from '../assets/profile/mark_shs_2.jpg'
 const Header = () => {
     const { cartItems } = useCart();
     return (
